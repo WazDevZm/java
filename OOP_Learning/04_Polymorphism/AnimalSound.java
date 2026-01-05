b162f1e017753032515d5ef6774bb7aa3161f44e
Wazingwa Mugala <wazingwamugala90@gmail.com>
2026-01-05 11:05:13 +0200

    Example of polymorphism

---
+++ OOP_Learning/04_Polymorphism/AnimalSound.java
@@ -0,0 +1,23 @@
+class Animal{
+    public void sound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal{
+    public void sound(){
+        System.out.println("Dog barks");
+    }
+}
+
+// this is the main class where the execution for this program starts
+
+class AnimalSound{
+    public static void main (String[] args){
+        Animal myAnimal = new Animal(); // Animal object
+        Animal myDog = new Dog(); // Dog object as Animal
+
+        myAnimal.sound(); // calls the method in Animal class
+        myDog.sound(); // calls the overridden method in Dog class
+    }
+}
