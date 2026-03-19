06ffc23e6103cbcded5ae5d7ca8a1e5384a73958
WazDevZm <wazingwamugala90@gmail.com>
2026-03-19 11:21:44 +0200

    Add launch configurations for Pattern2 and Pattern3 classes

--- REVIEW_ONE/Pattern2.java
+++ REVIEW_ONE/Pattern2.java
@@ -1,18 +1,22 @@
 
-package REVIEW_ONE;
-public class Pattern2 {
-    public static void main(String[] args) {
+package REVIEW_ONE; 
+public class Pattern2 
+
+
+
+{
+
+// we are writing this coe to be edu=ited at MIT one day
+
+    public static void main(String args[]){
         int n = 5;
         int num = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++) {
                 System.out.print(num + " ");
-                num++;
-            }
-            System.out.println();
+                num++; // this will increase the coundter by one;
+
+            } System.out.println();
         }
     }
-}
-
-// this code will be edited at MIT -2029
+} // this logic is correct and makes ultimate sense, very much!!!!
---
+++ REVIEW_ONE/Pattern3.java
@@ -0,0 +1,14 @@
+package REVIEW_ONE;
+
+public class Pattern3 {
+    public static void main(String args []){
+        int n = 5;
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++) {
+                System.out.print("* ");
+            } System.out.println();
+        }
+    }
+    
+}
+// the all starr pattern is the most basic pattern and it is very much used in many of the patterns, so it is very important to understand how to use it, and how to use the nested loops to print the stars in the right way.
