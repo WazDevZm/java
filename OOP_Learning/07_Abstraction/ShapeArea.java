8018aab497de8e51178d22acf1165265ec614b67
WazDevZm <wazingwamugala90@gmail.com>
2026-03-24 09:32:24 +0200

    feat: add more patterns to the code and repo.

--- OOP_Learning/07_Abstraction/ShapeArea.java
+++ OOP_Learning/07_Abstraction/ShapeArea.java
@@ -72,3 +72,4 @@ public class ShapeArea {
         }
     }
 }
+// I am not feleling well today and that...
--- REVIEW_ONE/Triangle1.java
+++ REVIEW_ONE/Triangle1.java
@@ -9,6 +9,13 @@ public class Triangle1 {
             }
             System.out.println();
         }
+        // another pattern going here
+        for (int i = 6; i >=1; i--){
+            for (int j = 1; j>=1;j--){
+                System.out.print(j + " ");
+            } System.out.println(" ");
+        }
     } 
     
 }
+// this is the pattern that will print out the reversal of the main sequence that was intially made;
