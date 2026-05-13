aa6c094c5e755626fb61c3bf49da3c8ca6e92ece
WazDevZm <wazingwamugala90@gmail.com>
2026-05-13 14:21:40 +0200

    feat : adding of two matrices.

--- Hash_Maps/task_1.java
+++ Hash_Maps/task_1.java
@@ -39,3 +39,4 @@ public class task_1 {
 }// using of the Hash maos to find the most common item in.  lsit of items in the hasbh a mas llist
 // using a for each loop and traversiiong through the system in the site of the work and th full work and the full 
 // frojr rin tuh the fllyer in the vlass in teh full styanec of the best sellter code, usit very simple/.....
+//usng the has map to get the mmoneth that we nned to eget the code running in the terminal as wee as the man feat we nned to oush to the cloud
---
+++ Program_1.java
@@ -0,0 +1,41 @@
+// remember that all importations must be placed outside all the classes and they are delcared in the first section of the progra,
+import java.util.Scanner;;
+public class Program_1 {
+    // this java program will help you add two matrices
+    public static void main(String[] args) {
+        // Create a new scanner object that will read user input
+        Scanner input = new Scanner(System.in);
+        // Prompt the user to enter the number of rows and columns for the matrices
+        System.out.print("Enter the number of rows:");
+        int rows = input.nextInt()
+        System.out.println("Enter the number of columns:");
+
+        int cols = input.nextInt();
+        // Create two 2d arryas that eill gold the inputs of the user
+
+        int first [] [] = new int [rows][cols];
+        int second [] [] = new int [rows][cols];
+
+        // create a 2d array that will hold the result of the addition
+
+        int sum [] [ ] = new int [rows][cols];
+        // prompt the user to enter the elements of the first matrix
+        System.out.println("Enter the elements of the first matrix");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                first [i] [j] = input.nextInt();
+            }
+        }
+        // Prompt the user to enter the elements of the second matrix.
+
+
+        
+
+    
+
+
+
+    }
+
+}
