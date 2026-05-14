34c1eae92192f82fc7b44de02691f2b42d93f09a
WazDevZm <wazingwamugala90@gmail.com>
2026-05-14 15:34:15 +0200

    feat : added the print out statements to the  code.

---
+++ Banker.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Banker {
+    public void static main (string [] args) {
+        System.out.println("Welcome to the Bank!"); // console applaiction
+        // Declare some variables here
+         double balance;
+         boolean isRunning = true;
+         int choice;
+
+        // this will giv out some data to the use 
+        System.out.println("********************************");
+        System.out.println("Banking Project");
+        System.out.println("1. Show Balance");
+        System.out.println("2.Deposit");
+        System.out.println("3.Withdraw");
+        System.out.println("4.Exit");
+        System.out.println("*********************************")
+        // Create a new Scanner ojedt that will read inpur from the user
+         Scanner input = new Scanner.(System.in);
+         int enterNumber = input.nextInt();
+    
+         
+         
+
+    
+
+        // Display Menu
+
+
+
+
+
+
+
+
+
+
+        // 
+    }   
+    
+}
